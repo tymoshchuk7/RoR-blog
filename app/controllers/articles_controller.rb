563df206685eaee1115fda73cfd61277d0f5a490
otymoshchuk <otymoshchuk@wemyit.com>
2022-12-26 18:16:21 +0200

    Articles Add tip for resources

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,16 @@
 class ArticlesController < ApplicationController
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+
   def index
     @articles = Article.all
   end
 
   def show
+    puts params
     @article = Article.find(params[:id])
   end
 
@@ -42,8 +49,4 @@ class ArticlesController < ApplicationController
     redirect_to root_path, status: :see_other
   end
 
-  private
-    def article_params
-      params.require(:article).permit(:title, :body)
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   root "articles#index"
 
-  # get "/articles", to: "articles#index"
-  # get "/articles/:id", to: "articles#show"
   resources :articles
+  #mapping routes for:
+  #   root GET    /                            articles#index
+  #   articles GET    /articles(.:format)          articles#index
+  #  new_article GET    /articles/new(.:format)      articles#new
+  #   article GET    /articles/:id(.:format)      articles#show
+  #   POST   /articles(.:format)          articles#create
+  # edit_article GET    /articles/:id/edit(.:format) articles#edit
+  #   PATCH  /articles/:id(.:format)      articles#update
+  #   DELETE /articles/:id(.:format)      articles#destroy
 end
