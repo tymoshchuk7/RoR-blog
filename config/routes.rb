e2830456b35340c8efd2f7ca1b441715084dc113
otymoshchuk <otymoshchuk@wemyit.com>
2022-12-27 20:14:58 +0200

    Update auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "articles#index"
-
-  resources :sessions, only: [:create]
 
   resources :articles do
     resources :comments
