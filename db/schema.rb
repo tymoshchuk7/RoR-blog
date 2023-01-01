320354eb5f7ae291ec50730ba26148e28feb1f83
otymoshchuk <otymoshchuk@wemyit.com>
2023-01-01 14:05:26 +0200

    Add first name and second name to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_27_151433) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_01_113532) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -48,6 +48,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_27_151433) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "second_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
